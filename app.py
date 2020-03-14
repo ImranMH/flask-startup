9ad4c8a9d63cad51db19ffddc462c668cb7f14b5
Imran Hossain <imran096@gmail.com>
2020-03-15 03:49:48 +0600

    setup template

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 # app.config["DEBUG"] = True
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
-    return "<h1>Hello Flask </h1>"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
